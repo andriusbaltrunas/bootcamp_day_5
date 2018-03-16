92e50c90882c1495316656f6ce4ca733528cb3c4
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-03-16 16:51:28 +0200

    enum

--- src/MyMainEnumExample.java
+++ src/MyMainEnumExample.java
@@ -9,5 +9,9 @@ public class MyMainEnumExample {
         for (Countries c : Countries.values()){
             System.out.println(c.name() + " "+ c.getLocale());
         }
+
+        Countries countries = Countries.valueOf("LT");
+
+        System.out.println("GAVAU-> " + countries.getLocale());
     }
 }
